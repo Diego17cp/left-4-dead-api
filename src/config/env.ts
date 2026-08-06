8ef7e40d275970b519388f5b3aa4aa7a3c2bcc46
Diego17 <diegoalbertocastropastor@gmail.com>
2026-08-05 21:33:09 -0500

    Add Zod for environment validation and create env.ts configuration file

---
+++ src/config/env.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+
+const envSchema = z.object({
+	PORT: z.coerce.number().default(3000),
+	DATABASE_URL: z.url(),
+	DIRECT_URL: z.url(),
+	NODE_ENV: z
+		.enum(["development", "production", "test"])
+		.default("development"),
+});
+
+export const env = envSchema.parse(process.env)
