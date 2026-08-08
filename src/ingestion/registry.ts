3e8f34c1a17a8e2b8873a3d9b58d5ec348a22d64
Diego17 <diegoalbertocastropastor@gmail.com>
2026-08-08 00:09:52 -0500

    Add campaign ingestion module to the registry

--- src/ingestion/registry.ts
+++ src/ingestion/registry.ts
@@ -1,5 +1,7 @@
+import { campaignIngestionModule } from "@/modules/campaigns/ingestion/campaign.module";
 import { gameIngestionModule } from "@/modules/games/ingestion/game.module";
 
 export const ingestionModules = [
   gameIngestionModule,
+  campaignIngestionModule
 ];
